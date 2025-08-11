572e1a2a6fed28212b9417288e570371dd7edf4b
Josef Friedrich <josef@friedrich.rocks>
2025-08-11 21:04:45 +0200

    Import all type definitions from https://github.com/Josef-Friedrich/LuaTeX_Lua-API/commit/a278b89090167826ceb75303b1b10cb1823b0a06

---
+++ examples/namespace.lua
@@ -0,0 +1,4 @@
+#! luametatex --fmt=/usr/local/texlive/2025/texmf-var/web2c/pdftex/mltex.fmt --permitloadlib
+local utils = require("utils")
+
+utils.print_global_namespace()
---
+++ examples/namespace_luaonly.lua
@@ -0,0 +1,4 @@
+#! luametatex --luaonly
+local utils = require("utils")
+
+utils.print_global_namespace()
