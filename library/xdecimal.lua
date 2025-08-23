9785c7dc2bc4ba3e4cd7568ec25c3fbc93a3dcff
Josef Friedrich <josef@friedrich.rocks>
2025-08-23 10:55:11 +0200

    Add function bodies to xdecimal

--- library/xdecimal.lua
+++ library/xdecimal.lua
@@ -1,1 +1,30 @@
----https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxdecimallib.c
+
+------https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxdecimallib.c
+xdecimal = {}
+
+function xdecimal.new() end
+
+function xdecimal.copy() end
+function xdecimal.trim() end
+function xdecimal.tostring() end
+function xdecimal.toengstring() end
+function xdecimal.tonumber() end
+function xdecimal.setprecision() end
+function xdecimal.getprecision() end
+
+function xdecimal.conj() end
+function xdecimal.abs() end
+function xdecimal.pow() end
+function xdecimal.sqrt() end
+function xdecimal.ln() end
+function xdecimal.log() end
+function xdecimal.exp() end
+function xdecimal.bor() end
+function xdecimal.bxor() end
+function xdecimal.band() end
+function xdecimal.shift() end
+function xdecimal.rotate() end
+function xdecimal.minus() end
+function xdecimal.plus() end
+function xdecimal.min() end
+function xdecimal.max() end
